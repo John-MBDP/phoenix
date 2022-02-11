92918a9bd8703006ba8eb604d151fbdd12cb8422
goingdust <dustinluck@gmail.com>
2022-02-11 16:26:03 -0500

    install superjson plugin to serialize the database date objects

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export const getServerSideProps = async () => {
+//   const articles = await prisma.articles.findMany();
+//   return {
+//     props: {
+//       initialArticles: articles,
+//     },
+//   };
+// };
 
 export default function Home() {
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -12,12 +25,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Hello</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +69,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
