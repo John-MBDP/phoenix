ab5cd7c5e64cfaa03f528be3d2c4fdfd24ad6cc4
meteora277 <justin.s.diaz@gmail.com>
2022-02-12 16:50:57 -0500

    add links to navbar

--- components/BottomNavigation/index.jsx
+++ components/BottomNavigation/index.jsx
@@ -1,24 +1,38 @@
 import {
   BottomNavigation,
-  Box,
-  BottomNavigationAction
+  BottomNavigationAction,
+  Link
 } from "@material-ui/core";
 import HomeIcon from "@mui/icons-material/Home";
 import MessageIcon from "@mui/icons-material/Message";
 import SearchIcon from "@mui/icons-material/Search";
 import styles from "../../styles/Home.module.css";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { useState } from "react";
+import { ButtonBase } from "@mui/material";
+import { useRouter } from "next/router";
 
-export default function BottomNav({ children }) {
+export default function BottomNav() {
+  const views = ["", "messages", "Search", "archive"];
+  const router = useRouter();
+
   return (
     <div className={styles.bottomNav}>
-      <BottomNavigationAction label="Recents" icon={<HomeIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<MessageIcon />} />
-      <BottomNavigationAction label="Archive" icon={<SearchIcon />} />
-      <BottomNavigationAction label="Archive" icon={<AccountCircleIcon />} />
+      <BottomNavigation
+        showLabels
+        sx={{ boxShadow: 3 }}
+        onChange={(event, newValue) => {
+          router.push(`/${views[newValue]}`);
+        }}
+      >
+        <BottomNavigationAction
+          label="Home"
+          icon={<HomeIcon />}
+        ></BottomNavigationAction>
+        <BottomNavigationAction label="Messages" icon={<MessageIcon />} />
+        <BottomNavigationAction label="Search" icon={<SearchIcon />} />
+        <BottomNavigationAction label="Archive" icon={<AccountCircleIcon />} />
+      </BottomNavigation>
     </div>
   );
 }
-BottomNav.link = ({ label, icon }) => {
-  return <div>AASDASDAS</div>;
-};
