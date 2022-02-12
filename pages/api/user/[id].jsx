0ce38fe3acfc40f204a7cdd9145a4e08b8ab72dc
meteora277 <justin.s.diaz@gmail.com>
2022-02-11 19:18:31 -0500

    create user params handler

---
+++ pages/api/user/[id].jsx
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  const { id } = req.query;
+
+  if (req.method === "GET") {
+    const user = await prisma.clients.findUnique({
+      where: {
+        id: Number(id)
+      }
+    });
+    res.status(200).json(user);
+  } else {
+    res.status(404);
+  }
+}
