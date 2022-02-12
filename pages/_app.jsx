def5489b24636100923f725125693777f34905dd
meteora277 <justin.s.diaz@gmail.com>
2022-02-12 15:17:28 -0500

    add bottom as persistant component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,15 @@
+import BottomNav from "../components/BottomNavigation";
 import "../styles/globals.css";
+import styles from "../styles/Home.module.css";
 
 // eslint-disable-next-line func-style
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <BottomNav />
+    </>
+  );
 }
 
 export default MyApp;
