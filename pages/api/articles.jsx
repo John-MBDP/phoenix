2af3af0a0ae66c54785f8f3a4e2bbbe7df5b5fec
goingdust <dustinluck@gmail.com>
2022-02-11 21:11:10 -0500

    Add order by desc to prisma query

--- pages/api/articles.jsx
+++ pages/api/articles.jsx
@@ -2,7 +2,13 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export default async function handler(req, res) {
   try {
-    const articles = await prisma.articles.findMany();
+    const articles = await prisma.articles.findMany({
+      orderBy: [
+        {
+          date: "desc",
+        },
+      ],
+    });
     res.status(200).json(articles);
   } catch {
     res.status(500).json({ error: "failed to load data" });
