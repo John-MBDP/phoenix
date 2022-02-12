b1ccb103a703453d99386eea637420cae5321c19
meteora277 <justin.s.diaz@gmail.com>
2022-02-11 22:50:10 -0500

    create larfirm api route

---
+++ pages/api/lawfirms.jsx
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+export default async function handler(req, res) {
+  try {
+    const articles = await prisma.lawfirms.findMany();
+    res.status(200).json(articles);
+  } catch {
+    res.status(500).json({ error: "failed to load data" });
+  }
+}
