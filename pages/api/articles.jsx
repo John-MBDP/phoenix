d33715612a830d89e412cc6ec7856298c73c0b36
meteora277 <justin.s.diaz@gmail.com>
2022-02-11 20:49:33 -0500

    remove whitespaces

--- pages/api/articles.jsx
+++ pages/api/articles.jsx
@@ -1,7 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
-
 export default async function handler(req, res) {
   try {
     const articles = await prisma.articles.findMany();
