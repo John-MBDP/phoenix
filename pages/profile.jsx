a45d298655bd210c1d27367da21431ff5d01d2d7
meteora277 <justin.s.diaz@gmail.com>
2022-02-13 00:38:05 -0500

    created profile view

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,7 +1,13 @@
 import RoundedTopContainer from "../components/RoundedTopContainer";
 
 const Profile = () => {
-  return <RoundedTopContainer>uwu</RoundedTopContainer>;
+  return (
+    <RoundedTopContainer
+      image={"/images/articles/forest.jpeg"}
+      alt={"forest"}
+      height={"600px"}
+    ></RoundedTopContainer>
+  );
 };
 
 export default Profile;
