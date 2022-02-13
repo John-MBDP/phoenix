b223eb0a550cd66545451f0eb8cdcf101a777500
meteora277 <justin.s.diaz@gmail.com>
2022-02-13 13:40:26 -0500

    add event listener to list item click

--- components/MenuItem/index.jsx
+++ components/MenuItem/index.jsx
@@ -8,29 +8,36 @@ import {
   IconButton
 } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-
+import { useRouter } from "next/router";
+import { ListItemButton } from "@mui/material";
 const MenuItem = ({ children, heading }) => {
+  const router = useRouter();
+
+  const handleRedirect = (event, page) => {
+    console.log(event);
+    console.log(page);
+    router.push(page);
+  };
+
   return (
-    <div>
-      <List className={styles.list}>
-        <ListItem
-          sx={{ pb: 1.8, pt: 0 }}
-          divider
-          secondaryAction={
-            <IconButton edge="end" aria-label="delete">
-              <ArrowForwardIosIcon fontSize="small" />
-            </IconButton>
-          }
-        >
-          <ListItemAvatar>
-            <Avatar sx={{ backgroundColor: "white", width: 30, height: 30 }}>
-              {children}
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary={heading} />
-        </ListItem>
-      </List>
-    </div>
+    <ListItemButton sx={{ width: "100%", borderRadius: "50%" }}>
+      <ListItem
+        sx={{ pb: 1.8, pt: 2 }}
+        divider
+        secondaryAction={
+          <IconButton edge="end" aria-label="delete">
+            <ArrowForwardIosIcon fontSize="small" />
+          </IconButton>
+        }
+      >
+        <ListItemAvatar>
+          <Avatar sx={{ backgroundColor: "white", width: 30, height: 30 }}>
+            {children}
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={heading} />
+      </ListItem>
+    </ListItemButton>
   );
 };
 
