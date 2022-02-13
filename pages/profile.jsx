22383ac656401cc260229863ab4d7f209bd591f3
meteora277 <justin.s.diaz@gmail.com>
2022-02-13 13:40:54 -0500

    surround menu items with a List component

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,6 +1,7 @@
 import RoundedTopContainer from "../components/RoundedTopContainer";
 import MenuItem from "../components/MenuItem";
 import Person from "@mui/icons-material/Person";
+import List from "@mui/material/List";
 import NotificationImportantIcon from "@mui/icons-material/NotificationImportant";
 import FaceIcon from "@mui/icons-material/Face";
 import LocalAtmIcon from "@mui/icons-material/LocalAtm";
@@ -10,21 +11,23 @@ import LogoutIcon from "@mui/icons-material/Logout";
 const Profile = () => {
   return (
     <RoundedTopContainer image={"/images/articles/forest.jpeg"} alt={"forest"}>
-      <MenuItem heading="Personal Information">
-        <FaceIcon sx={{ color: "black" }} />
-      </MenuItem>
-      <MenuItem heading="Notifications">
-        <NotificationImportantIcon sx={{ color: "black" }} />
-      </MenuItem>
-      <MenuItem heading="Payments">
-        <LocalAtmIcon sx={{ color: "black" }} />
-      </MenuItem>
-      <MenuItem heading="How does Phoenix Work?">
-        <ReportIcon sx={{ color: "black" }} />
-      </MenuItem>
-      <MenuItem heading="Log Out">
-        <LogoutIcon sx={{ color: "black" }} />
-      </MenuItem>
+      <List component="nav">
+        <MenuItem heading="Personal Information">
+          <FaceIcon sx={{ color: "black" }} />
+        </MenuItem>
+        <MenuItem heading="Notifications">
+          <NotificationImportantIcon sx={{ color: "black" }} />
+        </MenuItem>
+        <MenuItem heading="Payments">
+          <LocalAtmIcon sx={{ color: "black" }} />
+        </MenuItem>
+        <MenuItem heading="How does Phoenix Work?">
+          <ReportIcon sx={{ color: "black" }} />
+        </MenuItem>
+        <MenuItem heading="Log Out">
+          <LogoutIcon sx={{ color: "black" }} />
+        </MenuItem>
+      </List>
     </RoundedTopContainer>
   );
 };
