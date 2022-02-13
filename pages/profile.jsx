393764fdd5e3199cbf37b4cb52647c412e89c0c3
meteora277 <justin.s.diaz@gmail.com>
2022-02-13 02:48:40 -0500

    create menu items

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,12 +1,31 @@
 import RoundedTopContainer from "../components/RoundedTopContainer";
+import MenuItem from "../components/MenuItem";
+import Person from "@mui/icons-material/Person";
+import NotificationImportantIcon from "@mui/icons-material/NotificationImportant";
+import FaceIcon from "@mui/icons-material/Face";
+import LocalAtmIcon from "@mui/icons-material/LocalAtm";
+import ReportIcon from "@mui/icons-material/Report";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const Profile = () => {
   return (
-    <RoundedTopContainer
-      image={"/images/articles/forest.jpeg"}
-      alt={"forest"}
-      height={"600px"}
-    ></RoundedTopContainer>
+    <RoundedTopContainer image={"/images/articles/forest.jpeg"} alt={"forest"}>
+      <MenuItem heading="Personal Information">
+        <FaceIcon sx={{ color: "black" }} />
+      </MenuItem>
+      <MenuItem heading="Notifications">
+        <NotificationImportantIcon sx={{ color: "black" }} />
+      </MenuItem>
+      <MenuItem heading="Payments">
+        <LocalAtmIcon sx={{ color: "black" }} />
+      </MenuItem>
+      <MenuItem heading="How does Phoenix Work?">
+        <ReportIcon sx={{ color: "black" }} />
+      </MenuItem>
+      <MenuItem heading="Log Out">
+        <LogoutIcon sx={{ color: "black" }} />
+      </MenuItem>
+    </RoundedTopContainer>
   );
 };
 
