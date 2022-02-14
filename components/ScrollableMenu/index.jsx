b96ddcdbbaf220dd5ae04f31c1097ae1180d1c5d
meteora277 <justin.s.diaz@gmail.com>
2022-02-14 14:25:02 -0500

    fix bug on tabs not having a value prop

--- components/ScrollableMenu/index.jsx
+++ components/ScrollableMenu/index.jsx
@@ -1,9 +1,23 @@
 import { Tabs, Tab } from "@mui/material";
+import { useState } from "react";
 
 const ScrollableMenu = ({ children }) => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    console.log(newValue);
+    setValue(newValue);
+  };
+
   return (
     <div>
-      <Tabs variant="scrollable" scrollButtons="auto" aria-label="scrollable auto tabs example">
+      <Tabs
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+        value={value}
+        onChange={handleChange}
+      >
         {children}
       </Tabs>
     </div>
