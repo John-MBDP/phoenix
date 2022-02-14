1920cef1200555280b226e4bf368881e81f27e59
meteora277 <justin.s.diaz@gmail.com>
2022-02-13 22:59:47 -0500

    replace Box with a div

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,6 +1,5 @@
 import RoundedTopContainer from "../components/RoundedTopContainer";
 import MenuItem from "../components/MenuItem";
-import Person from "@mui/icons-material/Person";
 import List from "@mui/material/List";
 import NotificationImportantIcon from "@mui/icons-material/NotificationImportant";
 import FaceIcon from "@mui/icons-material/Face";
@@ -13,13 +12,13 @@ import { Typography, Box } from "@material-ui/core";
 const Profile = () => {
   return (
     <RoundedTopContainer image={"/images/articles/forest.jpeg"} alt={"forest"}>
-      <UserStatCard />
+      <UserStatCard name="Hu Tao" />
 
-      <Box sx={{ pl: "1rem" }}>
+      <div style={{ paddingTop: "1rem" }}>
         <Typography component="h1" variant="h6" sx={{ color: "pink" }}>
           Account Settings
         </Typography>
-      </Box>
+      </div>
 
       <List component="nav">
         <MenuItem heading="Personal Information" path="/personal-info">
