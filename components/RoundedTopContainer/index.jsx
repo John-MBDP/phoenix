899f966320e058eab5168dd353bb53856bae0306
meteora277 <justin.s.diaz@gmail.com>
2022-02-14 15:35:28 -0500

    create header compound component

--- components/RoundedTopContainer/index.jsx
+++ components/RoundedTopContainer/index.jsx
@@ -1,7 +1,8 @@
+import { Typography } from "@material-ui/core";
 import Image from "next/image";
 import styles from "./index.module.css";
 
-const RoundedTopContainer = ({ children, image, alt, height }) => {
+const RoundedTopContainer = ({ children, image, alt, height, header }) => {
   return (
     <>
       <div className={styles.image}>
@@ -16,6 +17,16 @@ const RoundedTopContainer = ({ children, image, alt, height }) => {
         </div>
       </div>
     </>
+  );
+};
+
+RoundedTopContainer.Header = ({ text }) => {
+  return (
+    <div style={{ paddingTop: "16px" }}>
+      <Typography component="h1" variant="h6">
+        {text}
+      </Typography>
+    </div>
   );
 };
 
