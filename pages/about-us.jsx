fb751bd71adce16130e7086233f4339d4b02ffa3
meteora277 <justin.s.diaz@gmail.com>
2022-02-14 16:51:30 -0500

    create about us page

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -1,8 +1,40 @@
-const AboutUs = () => {
+import { Typography } from "@material-ui/core";
+import { useEffect } from "react";
+import RoundedTopContainer from "../components/RoundedTopContainer";
+import UserStatsCard from "../components/UserStatsCard";
+
+const AboutUs = ({ setHeader }) => {
+  useEffect(() => setHeader({ heading: "", hidden: true }), []);
+
   return (
-    <div>
-      <h1>About us</h1>
-    </div>
+    <RoundedTopContainer image="/images/book3.jpeg" alt="books" height="600px">
+      <UserStatsCard />
+      <RoundedTopContainer.Header text="How Does Phoenix Work?" />
+      <br />
+      <div>
+        <Typography variant="body1" component="h3">
+          <strong>Benefits</strong>
+        </Typography>
+        <Typography variant="body1" gutterBottom component="body1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          saepe fuga at omnis! Consequuntur necessitatibus iusto cum non totam
+          reiciendis soluta iste dolorem ipsum, animi cumque rem quaerat.
+          Consequuntur, excepturi.
+        </Typography>
+      </div>
+      <br />
+      <div>
+        <Typography variant="body1" component="h3">
+          <strong>Why Join Our Platform?</strong>
+        </Typography>
+        <Typography variant="body1" component="body1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          saepe fuga at omnis! Consequuntur necessitatibus iusto cum non totam
+          reiciendis soluta iste dolorem ipsum, animi cumque rem quaerat.
+          Consequuntur, excepturi.
+        </Typography>
+      </div>
+    </RoundedTopContainer>
   );
 };
 
