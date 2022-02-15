bfce39540d7cebddd2bcaf81ad25ee2a5436eb75
meteora277 <justin.s.diaz@gmail.com>
2022-02-14 21:42:47 -0500

    add card image to articles/:id page

--- pages/articles/[id].jsx
+++ pages/articles/[id].jsx
@@ -1,45 +1,58 @@
 import { PrismaClient } from "@prisma/client";
 import Timeago from "react-timeago";
 import Article from "../../components/Article";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import styles from "./index";
 
 const prisma = new PrismaClient();
 
-export const getServerSideProps = async context => {
+export const getServerSideProps = async (context) => {
   const id = Number(context.params.id);
   const article = await prisma.articles.findUnique({
     where: {
-      id,
-    },
+      id
+    }
   });
   return {
     props: {
-      article,
-    },
+      article
+    }
   };
 };
 
 const ArticleShow = ({ article }) => {
   return (
-    <section className="article-container">
-      <style jsx>{`
-        .article-container {
-          height: 500px;
-          overflow: scroll;
-          position: fixed;
-          bottom: 0;
-          box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-          border-top-left-radius: 50px;
-          border-top-right-radius: 50px;
-        }
-      `}</style>
-      <Article
-        id={article.id}
-        title={article.title}
-        author={article.author}
-        body={article.body}
-        date={<Timeago date={article.date} />}
-      />
-    </section>
+    <>
+      <Card className={styles.image}>
+        <CardMedia
+          component="img"
+          width="390"
+          image={article.image}
+          alt={article.image}
+        />
+      </Card>
+      <section className="article-container">
+        <style jsx>{`
+          .article-container {
+            height: 500px;
+            overflow: scroll;
+            position: fixed;
+            bottom: 0;
+            box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+            border-top-left-radius: 50px;
+            border-top-right-radius: 50px;
+          }
+        `}</style>
+        <Article
+          id={article.id}
+          title={article.title}
+          author={article.author}
+          body={article.body}
+          date={<Timeago date={article.date} />}
+        />
+      </section>
+    </>
   );
 };
 
