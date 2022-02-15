d834003eff165c3a68d0f4ea0c7636c3019ba8b4
meteora277 <justin.s.diaz@gmail.com>
2022-02-15 17:03:47 -0500

    switch css module to style jsx

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -1,17 +1,32 @@
-import Button from "@mui/material/Button";
-import styles from "./index.module.css";
-
-const StyledButton = ({ children, ...restprops }) => {
+const StyledButton = ({ children, color, onClick, ...restprops }) => {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <button
         {...restprops}
         variant="contained"
         size="large"
-        className={styles.button}
+        className="button"
+        onClick={() => onClick()}
       >
         {children}
       </button>
+      <style jsx>{`
+        .button {
+          background-color: ${color ? color : "#ff0056"};
+          color: white;
+          border: none;
+          margin-bottom: 2rem;
+          padding: 1rem 1.5rem;
+          border-radius: 0.3rem;
+          text-transform: uppercase;
+          font-weight: 800;
+        }
+
+        .container {
+          display: flex;
+          justify-content: flex-end;
+        }
+      `}</style>
     </div>
   );
 };
