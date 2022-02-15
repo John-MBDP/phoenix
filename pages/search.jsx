293132400b629539f239de7083e1c9891c0bfd3e
meteora277 <justin.s.diaz@gmail.com>
2022-02-15 13:19:41 -0500

    start search page markup

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,17 +1,34 @@
 import ScrollableMenu from "../components/ScrollableMenu";
-import { Tab } from "@mui/material";
+import { Tab, Box } from "@mui/material";
 import { useEffect } from "react";
+import SearchCard from "../components/SearchCard";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-const Search = ({ setHeader }) => {
+export const getServerSideProps = async () => {
+  const lawyers = await prisma.lawyers.findMany();
+
+  return {
+    props: {
+      lawyers
+    }
+  };
+};
+
+const Search = ({ setHeader, lawyers }) => {
   useEffect(() => setHeader({ header: "LAWYERS", hidden: false }), []);
+
   return (
-    <ScrollableMenu>
-      <Tab label="Item One" />
-      <Tab label="Item One" />
-      <Tab label="Item One" />
-      <Tab label="Item One" />
-      <Tab label="Item One" />
-    </ScrollableMenu>
+    <Box sx={{ px: "2rem" }}>
+      <ScrollableMenu>
+        <Tab label="Item One" />
+        <Tab label="Item One" />
+        <Tab label="Item One" />
+        <Tab label="Item One" />
+        <Tab label="Item One" />
+      </ScrollableMenu>
+      <SearchCard />
+    </Box>
   );
 };
 
