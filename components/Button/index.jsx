415b31c6414ee3a4fd2973ee939073a674074a58
meteora277 <justin.s.diaz@gmail.com>
2022-02-16 17:28:54 -0500

    add prop for conditional rendering

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -1,13 +1,19 @@
 import styles from "./index.module.css";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
-const StyledButton = ({ children, expanded = false, ...restprops }) => {
+const StyledButton = ({
+  children,
+  expanded = false,
+  background,
+  ...restprops
+}) => {
   return (
     <div className={styles.container}>
       <button
         {...restprops}
         variant="contained"
         size="large"
+        style={{ backgroundColor: background ? `${background}` : null }}
         className={
           expanded ? `${styles.button} ${styles.expanded}` : styles.button
         }
