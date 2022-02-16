98e6a08bf45308ea405aa214467c77bfc531cd56
meteora277 <justin.s.diaz@gmail.com>
2022-02-16 00:33:35 -0500

    create wide button component

--- pages/lawyer/[id].jsx
+++ pages/lawyer/[id].jsx
@@ -8,7 +8,8 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import PhoneIcon from "@mui/icons-material/Phone";
 import Button from "../../components/Button";
 import AnnouncementIcon from "@mui/icons-material/Announcement";
-
+import EmailIcon from "@mui/icons-material/Email";
+import Widebutton from "../../components/WideButton";
 export const getServerSideProps = async (context) => {
   const id = Number(context.params.id);
   console.log(id);
@@ -32,7 +33,8 @@ const Lawyer = ({ setHeader, lawyer }) => {
     address,
     date_certified,
     phone_number,
-    location
+    location,
+    email
   } = lawyer;
   useEffect(() => {
     setHeader((prev) => ({ ...prev, hidden: true }));
@@ -57,19 +59,40 @@ const Lawyer = ({ setHeader, lawyer }) => {
         </Button>
       </Box>
       <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <Typography variant="body2">
-          <LocationOnIcon fontSize="16px" />
+        <Typography variant="body1">
+          <LocationOnIcon fontSize="20" sx={{ mr: " 0.3rem" }} />
           {`${address}, ${location}`}
         </Typography>
 
-        <Typography variant="body2">
-          <PhoneIcon fontSize="16px" />
+        <Typography variant="body1">
+          <PhoneIcon fontSize="20px" sx={{ mr: " 0.3rem" }} />
           {phone_number}
         </Typography>
-        <Typography variant="body2">
+        <Typography variant="body1">
+          <EmailIcon fontSize="16px" sx={{ mr: " 0.3rem" }} />
+          {email}
+        </Typography>
+        <Typography variant="body1">
           <strong>Recognized Since: {date_certified.getFullYear()}</strong>
         </Typography>
       </Box>
+      <Widebutton
+        color="black"
+        outLineColor="#1B4463"
+        padding="0.5rem 0"
+        outline
+        strong
+        ammount="$550"
+      >
+        <div>
+          <Typography variant="button">ONE TIME SERVICE FEE</Typography>
+          <Typography variant="subtitle">click to see more</Typography>
+          <style jsx>{`
+            display: flex;
+            flex-direction: column;
+          `}</style>
+        </div>
+      </Widebutton>
     </RoundedTopContainer>
   );
 };
