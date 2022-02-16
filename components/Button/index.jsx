754bd673633912fbea6eb9aedae40778b7d6d3f2
meteora277 <justin.s.diaz@gmail.com>
2022-02-16 14:29:02 -0500

    remove duplicate button styles

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -14,38 +14,6 @@ const StyledButton = ({ children, expanded = false, ...restprops }) => {
       >
         {children}
       </button>
-
-      <style jsx>{`
-        .innerContainer {
-          display: flex;
-          justify-content: space-between;
-        }
-        .message {
-          margin-left: 0.3rem;
-          display: flex;
-          align-items: center;
-        }
-        .button {
-          background-color: "#ff0056";
-          color: white;
-          border: none;
-          margin-bottom: 2rem;
-          padding: "1rem 1.5rem";
-          border-radius: 0.3rem;
-          text-transform: uppercase;
-          font-weight: 800;
-          display: flex;
-          justify-content: space-between;
-          box-shadow: 0 5px 1rem rgba(0, 0, 0, 0.5);
-        }
-        .icon {
-        }
-        .container {
-          display: flex;
-          position: relative;
-          justify-content: "flex-end";
-        }
-      `}</style>
     </div>
   );
 };
