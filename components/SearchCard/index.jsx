401b12abdc6ae822b151f577dd6237ebbe317290
meteora277 <justin.s.diaz@gmail.com>
2022-02-16 14:30:11 -0500

    add link to lawyer in search card

--- components/SearchCard/index.jsx
+++ components/SearchCard/index.jsx
@@ -3,58 +3,59 @@ import CardMedia from "@mui/material/CardMedia";
 import { Box, Typography } from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import PhoneIcon from "@mui/icons-material/Phone";
+import Link from "next/link";
 
-const SearchCard = ({ fullName, location, phone, certified, image }) => {
+const SearchCard = ({ fullName, location, phone, certified, image, id }) => {
   return (
-    <Card
-      sx={{
-        mt: "0.5rem",
-        p: "0.7rem",
-        borderRadius: 1,
-        display: "flex",
-        boxShadow: 5
-      }}
-    >
-      <Box sx={{ borderRadius: 1 }}>
-        <CardMedia
-          sx={{
-            width: "110px",
-            height: "110px",
-            objectFit: "cover",
-            borderRadius: 1
-          }}
-          image={image}
-          alt="user-icon"
-        />
-      </Box>
-      <Box
+    <Link href={`/lawyer/${id}`}>
+      <Card
         sx={{
-          width: 1,
-          ml: "1rem",
+          mt: "0.5rem",
+          p: "0.7rem",
+          borderRadius: 1,
           display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-evenly"
+          boxShadow: 5
         }}
       >
-        <Typography variant="body1">
-          <strong>{fullName}</strong>
-        </Typography>
-        <br />
-
-        <Typography variant="body2">
-          <LocationOnIcon fontSize="16px" />
-          {location}
-        </Typography>
-
-        <Typography variant="body2">
-          <PhoneIcon fontSize="16px" />
-          {phone}
-        </Typography>
-        <Typography variant="body2">
-          <strong>Recognized Since: {certified}</strong>
-        </Typography>
-      </Box>
-    </Card>
+        <Box sx={{ borderRadius: 1 }}>
+          <CardMedia
+            sx={{
+              width: "110px",
+              height: "110px",
+              objectFit: "cover",
+              borderRadius: 1
+            }}
+            image={image}
+            alt="user-icon"
+          />
+        </Box>
+        <Box
+          sx={{
+            width: 1,
+            ml: "1rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly"
+          }}
+        >
+          <Typography variant="body1">
+            <strong>{fullName}</strong>
+          </Typography>
+          <br />
+          <Typography variant="body2">
+            <LocationOnIcon fontSize="16px" />
+            {location}
+          </Typography>
+          <Typography variant="body2">
+            <PhoneIcon fontSize="16px" />
+            {phone}
+          </Typography>
+          <Typography variant="body2">
+            <strong>Recognized Since: {certified}</strong>
+          </Typography>
+        </Box>
+      </Card>
+    </Link>
   );
 };
 
