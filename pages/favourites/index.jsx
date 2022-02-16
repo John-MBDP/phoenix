a0c7c1cebfe978fd50d5acd44f8db873efc86ca4
goingdust <dustinluck@gmail.com>
2022-02-16 15:43:09 -0500

    Bring in articles and render them depending on the status of favourites state

--- pages/favourites/index.jsx
+++ pages/favourites/index.jsx
@@ -1,9 +1,12 @@
 import { Tab, Tabs } from "@material-ui/core";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import sessionOptions from "../../lib/session";
 import { withIronSessionSsr } from "iron-session/next";
 import { PrismaClient } from "@prisma/client";
-import styles from "./index.module.scss";
+import styles from "../../styles/Home.module.css";
+import { BottomNavigationContext } from "../../Contexts/BottomNavigationContext";
+import ArticleCard from "../../components/ArticleCard";
+import Timeago from "react-timeago";
 
 const prisma = new PrismaClient();
 
@@ -55,40 +58,54 @@ const Favourites = ({
   lawyerFavourites,
   lawfirmFavourites,
 }) => {
+  const ARTICLES = "ARTICLES";
+  const LAWYERS = "LAWYERS";
+  const LAWFIRMS = "LAWFIRMS";
+
+  const { setActive } = useContext(BottomNavigationContext);
   const [value, setValue] = useState(0);
-  const [favourites, setFavourites] = useState({
-    articles: articleFavourites,
-    lawyers: lawyerFavourites,
-    lawfirms: lawfirmFavourites,
-  });
+  const [favourites, setFavourites] = useState(ARTICLES);
 
   useEffect(() => {
     setHeader({ header: "FAVOURITES", hidden: false });
+    setActive(true);
   }, []);
 
   const handleChange = (e, value) => {
     setValue(value);
   };
 
-  console.log(articleFavourites);
+  const parsedArticleFavourites = articleFavourites.map(favourite => {
+    return (
+      <ArticleCard
+        key={favourite.id}
+        articleId={favourite.article_id}
+        title={favourite.articles.title}
+        body={favourite.articles.body.slice(0, 100) + "..."}
+        date={<Timeago date={favourite.articles.date} />}
+        image={favourite.articles.image}
+      />
+    );
+  });
 
   return (
-    <div className={styles.favourites}>
+    <div className={styles.container}>
       <Tabs
         value={value}
         indicatorColor="secondary"
         textColor="secondary"
         onChange={handleChange}
       >
-        <Tab label="Articles" onClick={() => setMessageCards(lawyerMessages)} />
-        <Tab label="Lawyers" onClick={() => setMessageCards(lawyerMessages)} />
+        <Tab label="Articles" onClick={() => setFavourites(ARTICLES)} />
+        <Tab label="Lawyers" onClick={() => setFavourites(LAWYERS)} />
         <Tab
           label="Lawfirms"
           onClick={() => {
-            setMessageCards(lawfirmMessages);
+            setFavourites(LAWFIRMS);
           }}
         />
       </Tabs>
+      {favourites === ARTICLES && parsedArticleFavourites}
     </div>
   );
 };
