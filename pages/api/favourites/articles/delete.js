5113ae2f6b14b56f1639ecf829729a9ba00e6a3c
goingdust <dustinluck@gmail.com>
2022-02-16 17:14:16 -0500

    Add client_id to article fave delete and seen_client column to messagesmodel

--- pages/api/favourites/articles/delete.js
+++ pages/api/favourites/articles/delete.js
@@ -12,6 +12,9 @@ export default async function handler(req, res) {
         article_id: {
           equals: favourite.article_id,
         },
+        client_id: {
+          equals: favourite.client_id,
+        },
       },
     });
     res.status(200).json(destroyedFavourite);
