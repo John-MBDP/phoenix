4849b0cd74ae2d433b27fa2bac1d0208ad19ac3d
meteora277 <justin.s.diaz@gmail.com>
2022-02-17 11:57:08 -0500

    add api call to /api/location to get lawyer by location

--- pages/search.jsx
+++ pages/search.jsx
@@ -16,6 +16,7 @@ export const getServerSideProps = async () => {
 };
 
 const Search = ({ setHeader, lawyers }) => {
+  const [currentLawyers, setCurrentLaywers] = useState(lawyers);
   const [city, setCity] = useState("");
   useEffect(() => {
     setHeader({ header: "LAWYERS", hidden: false });
@@ -26,10 +27,12 @@ const Search = ({ setHeader, lawyers }) => {
       console.log(data);
       const { latitude, longitude } = data.coords;
 
-      const cityData = await fetch("/api/location", {
-        method: "POST",
-        body: JSON.stringify({ latitude: latitude, longitude: longitude })
-      });
+      const cityData = await fetch(
+        `/api/location?latitude=${latitude}&longitude=${longitude}`,
+        {
+          method: "POST"
+        }
+      );
       const parsedCitydata = await cityData.json();
       console.log(parsedCitydata.location);
       setCity(parsedCitydata.location);
@@ -41,33 +44,47 @@ const Search = ({ setHeader, lawyers }) => {
     navigator.geolocation.getCurrentPosition(success, error);
   };
 
-  const lawyersArray = lawyers.map((lawyer) => {
-    return (
-      <SearchCard
-        key={lawyer.id}
-        fullName={`${lawyer.first_name} ${lawyer.last_name}`}
-        location={lawyer.location}
-        certified={lawyer.date_certified.getFullYear()}
-        phone={lawyer.phone_number}
-        image={lawyer.profile_pic}
-        id={lawyer.id}
-      />
-    );
+  const getLawyers = async (location) => {
+    const response = await fetch(`/api/lawyers?location=${location}`);
+    const parsedResponse = await response.json();
+    setCurrentLaywers(parsedResponse || {});
+  };
+
+  const lawyersArray = currentLawyers.map((lawyer) => {
+    if (lawyer) {
+      return (
+        <SearchCard
+          key={lawyer.id}
+          fullName={`${lawyer.first_name} ${lawyer.last_name}`}
+          location={lawyer.location}
+          certified={JSON.stringify(lawyer.date_certified)}
+          phone={lawyer.phone_number}
+          image={lawyer.profile_pic}
+          id={lawyer.id}
+        />
+      );
+    }
   });
+  const handleSubmit = (e, city) => {
+    e.preventDefault();
+    getLawyers(city);
+    console.log(e);
+  };
 
   return (
     <Box sx={{ px: "2rem", mt: "5rem" }}>
       <br />
       <div>
         <button onClick={() => getGeoLocation()}>Get Location</button>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e, city)}>
           <TextField
             fullWidth
-            name="firstName"
             value={city}
             onChange={(e) => setCity(e.target.value)}
             margin="normal"
+            autoComplete="off"
           />
+          <button type="submit">submit</button>
         </form>
       </div>
       <ScrollableMenu>
