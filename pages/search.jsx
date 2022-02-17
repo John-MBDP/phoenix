7f01fe83e9aabdcae516eaf9354ecef029eba73f
meteora277 <justin.s.diaz@gmail.com>
2022-02-17 03:06:42 -0500

    add getGeolocation function

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,6 +1,6 @@
 import ScrollableMenu from "../components/ScrollableMenu";
-import { Tab, Box } from "@mui/material";
-import { useEffect } from "react";
+import { Tab, Box, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
 import SearchCard from "../components/SearchCard";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
@@ -16,9 +16,30 @@ export const getServerSideProps = async () => {
 };
 
 const Search = ({ setHeader, lawyers }) => {
+  const [city, setCity] = useState("");
   useEffect(() => {
     setHeader({ header: "LAWYERS", hidden: false });
   }, []);
+
+  const getGeoLocation = async () => {
+    const success = async (data) => {
+      console.log(data);
+      const { latitude, longitude } = data.coords;
+
+      const cityData = await fetch("/api/location", {
+        method: "POST",
+        body: JSON.stringify({ latitude: latitude, longitude: longitude })
+      });
+      const parsedCitydata = await cityData.json();
+      console.log(parsedCitydata.location);
+      setCity(parsedCitydata.location);
+    };
+    const error = (err) => {
+      console.log(err.code);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error);
+  };
 
   const lawyersArray = lawyers.map((lawyer) => {
     return (
@@ -36,6 +57,19 @@ const Search = ({ setHeader, lawyers }) => {
 
   return (
     <Box sx={{ px: "2rem", mt: "5rem" }}>
+      <br />
+      <div>
+        <button onClick={() => getGeoLocation()}>Get Location</button>
+        <form>
+          <TextField
+            fullWidth
+            name="firstName"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            margin="normal"
+          />
+        </form>
+      </div>
       <ScrollableMenu>
         <Tab label="Injury" />
         <Tab label="Immigration" />
