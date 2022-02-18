5a22c83720a7757e959d92850545948348c7c883
John <johnartuz@hotmail.com>
2022-02-17 21:23:09 -0500

    Add new component to handle confirmation for sms verification

---
+++ pages/confirmation.jsx
@@ -0,0 +1,91 @@
+import RoundedTopContainer from "../components/RoundedTopContainer";
+import Typography from "@mui/material/Typography";
+import { useEffect } from "react";
+import Stack from "@mui/material/Stack";
+import Button from "../components/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+
+const Confirmation = ({ setHeader, setNavbar }) => {
+  useEffect(() => {
+    setHeader({ header: "", hidden: true }), setNavbar({ navbar: "", hidden: false });
+  }, []);
+  return (
+    <RoundedTopContainer image={"/sms-2.png"} alt={"signup-image"} height="350px">
+      <Typography variant="h5" sx={{ fontWeight: "800px", mb: 2, color: "#1D1F37" }}>
+        Confirmation
+      </Typography>
+      <Stack spacing={1}>
+        <Typography paragraph sx={{ color: "#1D1F37", fontWeight: "800px" }}>
+          Please Type the verification code sent to +0123456789
+        </Typography>
+        <Box
+          sx={{
+            width: "100",
+            height: "70px",
+            backgroundColor: "#1D1F37",
+            borderRadius: "5px",
+            boxShadow: 5,
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <Card
+            sx={{
+              width: "50px",
+              height: "50px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <TextField value="0" />
+          </Card>
+          <Card
+            sx={{
+              width: "50px",
+              height: "50px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <TextField value="9" />
+          </Card>
+          <Card
+            sx={{
+              width: "50px",
+              height: "50px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <TextField value="2" />
+          </Card>
+          <Card
+            sx={{
+              width: "50px",
+              height: "50px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <TextField value="6" />
+          </Card>
+        </Box>
+        <Typography paragraph fontWeight sx={{ color: "#FF0056" }}>
+          Resend code.
+        </Typography>
+      </Stack>
+      <Button>Confirm</Button>
+    </RoundedTopContainer>
+  );
+};
+
+export default Confirmation;
