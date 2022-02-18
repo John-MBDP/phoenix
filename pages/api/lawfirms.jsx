080c7d391b59f6999537fac4c0836dbbb8cb2c1e
meteora277 <justin.s.diaz@gmail.com>
2022-02-18 13:47:57 -0500

    remove console.logs

--- pages/api/lawfirms.jsx
+++ pages/api/lawfirms.jsx
@@ -4,7 +4,6 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   const { field, location } = req.query;
-  console.log(field, location, "uwu");
   if (req.method !== "GET") {
     res.status(400).json({ error: "We do not currently support this Method" });
     return;
@@ -34,12 +33,9 @@ export default async function handler(req, res) {
       return { ...item.lawfirms };
     });
 
-    console.log(parsedLawfirms, "uwu humui");
     res.status(200).json(parsedLawfirms);
     return;
   } else if (field !== "null" && location === "null") {
-    console.log(field, location, "potato");
-
     const filteredByField = await prisma.lawfirm_fields.findMany({
       where: {
         fields_of_law: {
@@ -57,7 +53,7 @@ export default async function handler(req, res) {
     const parsedLawfirms = filteredByField.map((item) => ({
       ...item.lawfirms
     }));
-    console.log(parsedLawfirms);
+
     res.json(parsedLawfirms);
     return;
   } else if (location !== "null" && field === "null") {
@@ -70,13 +66,11 @@ export default async function handler(req, res) {
       }
     });
 
-    console.log(lawfirmsFromLocation, location);
     res.status(200).json(lawfirmsFromLocation);
     return;
   } else {
-    console.log(field, location, "huehueheuhuehueehu");
     const lawfirmsFromLocation = await prisma.lawfirms.findMany();
-    console.log(lawfirmsFromLocation);
+
     res.status(200).json(lawfirmsFromLocation);
     return;
   }
