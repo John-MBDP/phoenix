96a83f537eab574c18cd215acdfaa2c7ee359663
goingdust <dustinluck@gmail.com>
2022-02-18 12:32:49 -0500

    Fill out the Notifications Provider with helper functions

---
+++ provider/NotificationsProvider.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+export const notificationsContext = createContext();
+
+export default function NotificationsProvider() {
+  const [notifications, setNotifications] = useState(0);
+
+  const addNotification = () => {
+    setNotifications(prev => prev + 1);
+  };
+
+  const clearNotifications = () => {
+    setNotifications(0);
+  };
+
+  const notificationsData = { notifications, addNotification, clearNotifications };
+
+  return (
+    <notificationsContext.Provider value={notificationsData}>
+      {props.children}
+    </notificationsContext.Provider>
+  );
+}
