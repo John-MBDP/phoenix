167436901bf386d096e45d4c1a5560ca7f743be7
meteora277 <justin.s.diaz@gmail.com>
2022-02-19 01:15:07 -0500

    remove served image sizes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["assets.example.com"],
-    deviceSizes: [390, 640, 750, 828, 1080],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384]
-  }
+  reactStrictMode: true
 };
