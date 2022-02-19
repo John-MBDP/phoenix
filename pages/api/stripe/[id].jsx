f9c322e1f536405deb14e2f76426ee679df85a95
meteora277 <justin.s.diaz@gmail.com>
2022-02-19 12:36:50 -0500

    add check stripe session api call

--- pages/api/stripe/[id].jsx
+++ pages/api/stripe/[id].jsx
@@ -6,7 +6,10 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 export default async (req, res) => {
   const { id } = req.query;
 
-  const session = await stripe.checkout.sessions.retrieve(id);
+  console.log(id);
+  const session = await stripe.checkout.sessions.retrieve(id, {
+    expand: ["payment_intent"]
+  });
 
-  res.status(200).json({ sessionId: session.id });
+  res.status(200).json({ session });
 };
