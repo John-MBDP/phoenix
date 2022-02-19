86cbea664de6884fdc950f3246ce45b046539b8a
meteora277 <justin.s.diaz@gmail.com>
2022-02-19 12:38:45 -0500

    create temporary success page

--- pages/stripe/purchase.jsx
+++ pages/stripe/purchase.jsx
@@ -1,12 +1,39 @@
 import { useRouter } from "next/router";
-import { loadStripe } from "@stripe/stripe-js";
+import useSWR from "swr";
 
-export default async () => {
+const potato = () => {
   const router = useRouter();
 
+  const { session_id } = router.query;
+  console.log(session_id);
+
+  const { data, error } = useSWR(
+    router.query.session_id ? `/api/stripe/${session_id}` : null,
+    (url) => fetch(url).then((res) => res.json())
+  );
+  // const {} = data;
+  /*
+    get session
+    id , amount_subtotal,currency, customer_details( email), 
+
+    payment_intent.charges.data.billing_details.address / country postal code
+                                                name
+                                reciept_url
+*/
   return (
     <div>
-      <h1>hi</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <br />
+      <br />
+      <br />
+      <pre>{data ? JSON.stringify(data, null, 2) : "Loading..."}</pre>
+      <h1>Potato</h1>
     </div>
   );
 };
+
+export default potato;
