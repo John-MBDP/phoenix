3b68a44cfbd9212ff9cb794d3513b35dcb37ec4c
meteora277 <justin.s.diaz@gmail.com>
2022-02-19 16:06:16 -0500

    add more prices and subscription types

--- pages/api/stripe/session.jsx
+++ pages/api/stripe/session.jsx
@@ -1,33 +1,38 @@
+import { PrismaClient } from "@prisma/client";
 import Stripe from "stripe";
 console.log(process.env.STRIPE_SECRET_KEY);
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2020-08-27"
 });
+const prisma = new PrismaClient();
 
 export default async (req, res) => {
-  const { quantity } = req.body;
+  const { quantity, price, paymentType } = req.body;
+  console.log(quantity, price);
+
+  console.log(typeof Number(price));
+
+  const product = await prisma.product_prices.findFirst({
+    where: {
+      price: Number(price)
+    }
+  });
+  console.log(product);
+
   const session = await stripe.checkout.sessions
     .create({
+      mode: paymentType,
       payment_method_types: ["card"],
       line_items: [
         {
-          price: process.env.PRICE_ID,
+          price: product.price_id,
           quantity
         }
       ],
-      mode: "payment",
-      success_url: `${req.headers.origin}/stripe/purchase?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${req.headers.origin}/stripe/?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${req.headers.origin}/checkout`
     })
     .then((data) => {
       res.status(200).json({ sessionId: data.id });
     });
 };
-// one time
-// 50 - price_1KUj1UC92AcJPbGeqX65kMsi
-// 125 - price_1KUj1UC92AcJPbGeajS1Vtrk
-// 200 - price_1KUj1UC92AcJPbGekgGRwLSO
-
-// 550 - price_1KUj2ZC92AcJPbGeBebPXPNF
-// 750 - price_1KUj2ZC92AcJPbGecv0Yx1w0
-// 1000 - price_1KUj2ZC92AcJPbGeJi6AEF58
--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -1,34 +1,58 @@
 import { loadStripe } from "@stripe/stripe-js";
+import { useEffect, useState } from "react";
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
 );
 
 const Checkout = () => {
-  const handleClick = async (e) => {
-    const { sessionId } = await fetch("/api/stripe/session", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify({ quantity: 1 })
-    }).then((data) => data.json());
+  const [price, setPrice] = useState("");
+  const [paymentType, setPaymentType] = useState("payment");
 
-    const stripe = await stripePromise;
-    const { error } = await stripe
-      .redirectToCheckout({
-        sessionId
-      })
-      .then((res) => res.json())
-      .catch((err) => console.log(err, error));
+  const handleClick = async (e) => {
+    console.log(e);
+    setPrice(e.target.value);
+    setPaymentType(e.target.name);
   };
+  useEffect(async () => {
+    if (price) {
+      const { sessionId } = await fetch("/api/stripe/session", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({ quantity: 1, price, paymentType })
+      }).then((data) => data.json());
+
+      const stripe = await stripePromise;
+      const { error } = await stripe
+        .redirectToCheckout({
+          sessionId
+        })
+        .then((res) => res.json())
+        .catch((err) => console.log(err, error));
+    }
+  }, [price]);
 
   return (
     <div>
       <br />
       <h1>Checkout</h1>
-      <button role="link" onClick={handleClick}>
+      <button role="link" value={125} name="payment" onClick={handleClick}>
         checkout
       </button>
+      <button role="link" value={50} name="payment" onClick={handleClick}>
+        checkout
+      </button>
+      <button role="link" value={200} name="payment" onClick={handleClick}>
+        checkout
+      </button>
+      <button role="link" value={550} name="subscription" onClick={handleClick}>
+        checkout
+      </button>
+      <button role="link" name="subscription" value={750} onClick={handleClick}>
+        checkout
+      </button>
+      <button name="potototo" onClick={(e) => console.log(e, price)}></button>
     </div>
   );
 };
