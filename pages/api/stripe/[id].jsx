999d27ebf05facbce57aa36f10e4b48f01b21d32
meteora277 <justin.s.diaz@gmail.com>
2022-02-19 17:56:50 -0500

    add Iron session to grab user information

--- pages/api/stripe/[id].jsx
+++ pages/api/stripe/[id].jsx
@@ -1,9 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+import sessionOptions from "../../../lib/session";
+import { withIronSessionApiRoute } from "iron-session/next";
+const prisma = new PrismaClient();
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2020-08-27"
 });
 
-export default async (req, res) => {
+export default withIronSessionApiRoute(async function (req, res) {
+  const user = req.session.user;
+  console.log(user);
   const { id } = req.query;
 
   console.log(id);
@@ -11,5 +17,9 @@ export default async (req, res) => {
     expand: ["payment_intent"]
   });
 
+  if (session.payment_status === "paid") {
+    console.log(session.client_reference_id);
+  }
+
   res.status(200).json({ session });
-};
+}, sessionOptions);
