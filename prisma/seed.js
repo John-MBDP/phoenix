295fceb5efe061aef1836d718256ca6bd0336391
goingdust <dustinluck@gmail.com>
2022-02-22 21:31:56 -0500

    Update dusty pic icon

--- prisma/seed.js
+++ prisma/seed.js
@@ -204,7 +204,7 @@ async function main() {
         phone_number: "705-739-5555",
         address: "55 Yonge St, Toronto, ON M6E 5A5",
         rating: 38,
-        profile_pic: "/images/lawyers/dusty-profile.jpg"
+        profile_pic: "/images/lawyers/dusty_profile_1.png"
       },
       {
         first_name: "John",
